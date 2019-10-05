480f28b43df430a9ee180e111f666e1e565e94dd
holishing <holishingtest@gmail.com>
2019-10-05 19:08:37 +0800

    update autotool commands to autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -eux
 
-test -e $(which aclocal)  || echo "Please install aclocal  !!"
-test -e $(which automake) || echo "Please install automake !!"
-test -e $(which autoconf) || echo "Please install autoconf !!"
+test -e $(which autoreconf)  || echo "Please install GNU Autotools!!"
 
-aclocal \
-&& automake --add-missing \
-&& autoconf
+autoreconf --install --force
+rm -rf autom4te.cache

6bd888f9050c545cae5b313bb48cc88da18a43cf
sean0921 <sean.li.shin.ho@gmail.com>
2020-04-07 21:08:38 +0800

    let Meson build system uses windres resource compiler

---
+++ windres/genrc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+## POSIX shell script for genenrate files for resources compilers (e.g. windres)
+
+for i in bern2time comfilt comfilt_trend fit getpl plot_ts raw remove_antenna remove_period remove_trend
+do cat > "$i".rc << EOF
+#include "version.rc"
+id ICON "$i.ico"
+EOF
+done

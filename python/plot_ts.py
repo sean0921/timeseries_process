b80c636ae30f3dd4e12cbec277ed22cd8ac33e8c
sean0921 <sean.li.shin.ho@gmail.com>
2020-04-23 00:15:20 +0800

    python: start works of refactoring code by python

---
+++ python/plot_ts.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print('                            PROGRAM PLOT_TS                            ')
+print('                                            by Kuo-En Ching 2005.01.11 ')
+print('Seismology Lab, Department of Earth Sciences, NCKU   Update 2020.04.08+')
+print('                                                                       ')
+print('source code: https://github.com/sean0921/timeseries_process            ')
+print('                                                                       ')
